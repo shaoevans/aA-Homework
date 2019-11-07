baebab1cce7e11f5ecce826923b6c1906fe08040
Evans Shao <evanzshao@gmail.com>
2019-11-07 10:46:35 -0800

    RSPEC HW

--- W4D4/rspec/spec/dessert_spec.rb
+++ W4D4/rspec/spec/dessert_spec.rb
@@ -6,37 +6,75 @@ Instructions: implement all of the pending specs (the `it` statements without bl
 =end
 
 describe Dessert do
-  let(:chef) { double("chef") }
+  let(:chef) { double("chef", name: "Evans") }
+  let(:brownie) { Dessert.new("brownie", 100, chef)}
 
   describe "#initialize" do
-    it "sets a type"
+    it "sets a type" do
+      expect(brownie.type).to eq("brownie")
+    end
 
-    it "sets a quantity"
+    it "sets a quantity" do 
+      expect(brownie.quantity).to eq(100)
+    end
 
-    it "starts ingredients as an empty array"
-
-    it "raises an argument error when given a non-integer quantity"
+    it "starts ingredients as an empty array" do
+      expect(brownie.ingredients).to be_empty
+    end
+    it "raises an argument error when given a non-integer quantity" do
+      expect { Dessert.new("cake", "100", chef)}.to raise_error(ArgumentError)
+    end
   end
 
   describe "#add_ingredient" do
-    it "adds an ingredient to the ingredients array"
+    before(:each) do 
+      brownie.add_ingredient("chocolate")
+    end
+    it "adds an ingredient to the ingredients array" do
+      expect(brownie.ingredients).to eq(["chocolate"])
+    end
+
   end
 
   describe "#mix!" do
-    it "shuffles the ingredient array"
+    before(:each) do 
+      brownie.add_ingredient("chocolate")
+      brownie.add_ingredient("sugar")
+      brownie.add_ingredient("spice")
+      brownie.add_ingredient("butter")
+      brownie.mix!
+    end
+    it "shuffles the ingredient array" do
+      expect(brownie.ingredients).to_not eq(["chocolate", "sugar", "spice", "butter"])
+    end
   end
 
   describe "#eat" do
-    it "subtracts an amount from the quantity"
+    before(:each) do
+      brownie.eat(1)
+    end
+    it "subtracts an amount from the quantity" do
+      expect(brownie.quantity).to eq(99)
+    end
 
-    it "raises an error if the amount is greater than the quantity"
+    it "raises an error if the amount is greater than the quantity" do
+      expect {brownie.eat(200)}.to raise_error
+    end
+
   end
 
   describe "#serve" do
-    it "contains the titleized version of the chef's name"
+    it "contains the titleized version of the chef's name" do
+      allow(chef).to receive(:titleize).and_return("Evans")
+      expect(brownie.serve).to eq("Evans has made 100 brownies!")
+    end
+    
   end
 
   describe "#make_more" do
-    it "calls bake on the dessert's chef with the dessert passed in"
+    it "calls bake on the dessert's chef with the dessert passed in" do
+      expect(chef).to receive(:bake).with(brownie)
+      brownie.make_more
+    end
   end
 end
